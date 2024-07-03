15a32116bd85f6e427190c630c2856b0578e1142
Niraj Kumar <nk659192@gmail.com>
2024-07-03 17:42:45 +0530

    add HomeRight

---
+++ frontend/src/Components/HomeRight.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const HomeRight = () => {
+  return (
+    <div className='text-white vsm:hidden lg:block lg:w-[350px] ml-12 pr-10 overflow-auto '>
+      <input type="text" className='input w-full rounded-3xl p-3 mt-1 bg-gray-800 focus:outline-none' />
+      <div className='w-full border-[1px] border-slate-800 h-40 mt-5 rounded-xl'>
+
+      </div>
+      <div className='w-full border-[1px] border-slate-800 h-[80%] mt-5 rounded-xl'>
+
+      </div>
+      <div className='w-full border-[1px] border-slate-800 h-60 mb-5 mt-5 rounded-xl'>
+      </div>
+    </div>
+  )
+}
+
+export default HomeRight
